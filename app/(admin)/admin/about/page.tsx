e2e89607765f383458a1bc76a70ff2939872b63d
Aryan8872 <gamersimpact778@gmail.com>
2025-12-07 02:50:46 +0545

    code fixes

--- app/(admin)/admin/about/page.tsx
+++ app/(admin)/admin/about/page.tsx
@@ -1,6 +1,8 @@
 import { getAboutUsContent } from "@/app/features/aboutus/service/aboutUsService";
 import AboutUsForm from "@/app/features/aboutus/ui/AboutUsForm";
 
+export const dynamic = "force-dynamic";
+
 export default async function AboutPage() {
   const aboutData = await getAboutUsContent();
 
